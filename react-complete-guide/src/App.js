a012ed7320524deebe6e19dabfed2b1382890735
Sriram Anne <sriram.anne2@gmail.com>
2023-04-18 10:30:31 -0400

    adding expense item component and basic css

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,19 @@
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  // doing the same as below in regular JS
+  // const paragraph = document.createElement('p');
+  // paragraph.textContent = 'This is a demo paragraph';
+  // document.getElementById('root').append(paragraph);
+
   return (
+    // this is advantage of React
     <div>
-      <h2> Let's get started.
-      </h2>
+      <h2> Let's get started.</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
+
   );
 }
 
