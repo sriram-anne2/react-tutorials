b10479fb83ac4945686e9891010f91c43374e0e3
Sriram Anne <sriram.anne2@gmail.com>
2023-04-18 11:14:50 -0400

    initial work with props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,21 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    //props has the 3 attributes that we sent to expenseItem in App.js
+    //basically props is a bundle of title, amount and date
+  // date object = year, month (array style), day
+
+
+//   const expenseDate = new Date(2023, 3, 18);
+//   const expenseTitle = "Car Insurance";
+//   const expenseAmount = 239.89;
+
   return (
-    <div className='expense-item'>
-      <div>April 18th 2023</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$239.89</div>
+    <div className="expense-item">
+      <div>{props.date.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
